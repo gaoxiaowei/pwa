4d3d307fd3b7b7100223ae73ba70a5301a13ee27
vincent <gaoxiaowei0629@163.com>
2025-03-03 14:09:37 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Caching assets');
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
